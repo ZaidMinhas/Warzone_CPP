c0cc97d961b66bef78f4cd61dfc2d59be946ee1f
AdamFarahat <91172205+AdamFarahat@users.noreply.github.com>
2024-09-18 18:56:52 -0400

    Set up blueprint, header file.

--- Orders/Orders.h
+++ Orders/Orders.h
@@ -4,19 +4,18 @@ class Order{
 public:
 	//Constructors
 	Order();
-	Order(string orderName);
+	Order(string orderName,Order* next);
 	Order(Order &orderCopy);
 	//methods
-	virtual bool validate();
-	virtual void execute();
+	virtual bool validate();//Will validate if the order can be executed during the order phase and the execution phase.
+	virtual void execute();//executes the order, this will be implemented in the subclasses
+	virtual string toString(); //Returns the details of the order to the player
 	//Accessors and Mutators
 	string getOrderName();
 	Order* getNext();
-	Order* getPrevious();
-	void setNext(Order &order);
-	void setPrevious(Order &previous);
+	void setNext(Order* next);
+	
 private:
 	string orderName;
 	Order* next;
-	Order* previous;
 };
--- Orders/OrdersList.h
+++ Orders/OrdersList.h
@@ -5,12 +5,21 @@ class OrdersList{
 public:
     //Constructors
     OrdersList();
-    OrdersList(Order* headOrder);
+    OrdersList(Order &firstOrder);
     OrdersList(OrdersList &list);
+    //Accessors and Mutators
+    Order* getHead();
+    Order* getCurrentOrder();
+    int getSize();
+    void setHead(Order* head);
+    void setCurrentOrder(Order* currentOrder);
     //methods
-    void move(string direction);
-    void remove();
-    void toString();
+    void addOrder(Order* order);//Will add an order to the end of the list.
+    void move(int oldposition,int newPosition);//Seeking the old position of the order and move it to the new position and putting it in between the order before and the order thats in the current position.
+    void remove(int position);//Seeking the order of the position and removing it from the list, if it is an order that requires a card, the card gets refunded.
+    void toString();//Displays all the orders in the players list.
 private:
-    Order* currentOrder; 
+    Order* head;
+    Order* currentOrder;
+    int size; 
 };
