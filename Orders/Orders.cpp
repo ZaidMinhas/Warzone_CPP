50cb59b45facc3f9db6bcd946e178d25fcabc6ed
Adam Farahat <farahatadam@gmail.com>
2024-10-02 12:56:15 -0400

    Make a simple template of validate and execute.

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "Orders.h"
 //Constructors
 Order::Order(){
@@ -10,14 +11,23 @@ Order::Order(string orderName){
 }
 //methods
 bool Order::validate(){
+    int random = rand()%10 + 1;
+    if(random<=5){
+        return false;
+    }
     return true;
 }
 
 void Order::execute(){
-    cout<<"Executing order: "<<getOrderName()<<endl;
+    if(this->validate()){
+        cout<<"Executing order: "<<this<<endl;
+    }
+    else{
+        cout<<"Unable to execute order: "<<this<<endl;
+    }
 }
 //operators
 ostream& operator<<(ostream &out, Order *o){
-    out<<"Order: "<<o->getOrderName();
+    out<<o->getOrderName();
     return out;
 }
