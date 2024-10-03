c22f641574aced1e2e3f0f63cec4bf4433c8b9e0
Adam Farahat <farahatadam@gmail.com>
2024-10-03 19:58:42 -0400

    Add testOrdersList to Main

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,13 @@
 #include "Map\MapDriver.h"
 #include "GameEngine\GameEngineDriver.h"
 #include "Player\PlayerDriver.h"
+#include "Orders\OrderDriver.h"
 int main(int argc, char **argv)
 {
 	testMap();
 	testPlayer();
 	testGameStates();
+	testOrdersList();
 	std::string s;
 	std::cin>>s;
 	return 0;
