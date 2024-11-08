1c684de5dcfd69a9f224f141806c3314d36aa9a2
Zaid Minhas <minhas.zaid@gmail.com>
2024-11-08 14:56:21 -0500

    Observer Code Done

---
+++ LoggingObserver/LoggingObserver.cpp
@@ -0,0 +1,32 @@
+//
+// Created by minha on 11/8/2024.
+//
+
+#include "LoggingObserver.h"
+#include <iostream>
+#include <fstream>
+
+
+void Subject::addObserver(Observer *observer) {
+  _observer = observer;
+}
+
+void Subject::Notify(const ILoggable& loggable){
+	_observer->Update(loggable);
+}
+
+LoggingObserver::LoggingObserver(){
+
+  std::ofstream myfile;
+  myfile.open("gamelog.txt");
+  myfile.close();
+}
+
+
+void LoggingObserver::Update(const ILoggable& loggable){
+	string log = loggable.stringToLog();
+  	std::ofstream myfile;
+    myfile.open("gamelog.txt", std::ios_base::app);
+    myfile << log;
+    myfile.close();
+}
