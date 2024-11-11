408ae902ed1ffb87effef3c1c124fb768f321274
Adam Farahat <farahatadam@gmail.com>
2024-11-10 19:34:58 -0500

    Fixed Player random ID bug

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -497,7 +497,7 @@ void GameEngine::startupPhase()
                 string name;
                 name = args.at(1);
                 playerList.push_back(new Player(name, this->playerCount));
-                this->playerCount++;
+                *(this->playerCount) = *(this->playerCount)+1;
                 transition(new PlayersAdded());
             }
             else if (args.at(0) == "gamestart")
