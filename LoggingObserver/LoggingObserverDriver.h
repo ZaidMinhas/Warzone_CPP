f1ca8d7b1e3e2a7967cbd525a5c17d25e452cd19
Zaid Minhas <minhas.zaid@gmail.com>
2024-11-10 20:17:15 -0500

    Done

---
+++ LoggingObserver/LoggingObserverDriver.h
@@ -0,0 +1,14 @@
+//
+// Created by minha on 11/10/2024.
+//
+
+#ifndef LOGGINGOBSERVERDRIVER_H
+#define LOGGINGOBSERVERDRIVER_H
+
+
+
+void testLoggingObserver();
+
+
+
+#endif //LOGGINGOBSERVERDRIVER_H
